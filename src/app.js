2be9c7b8498c0110a25ef0fecfb3e4c62394dadb
Chung Leung <chungleu@uk.ibm.com>
2019-07-11 10:05:08 +0100

    allow cors

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const application = () => {
   const app = express();
 
   app.use(bodyParser.json());
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    next();
+  });
   app.use('/', router());
 
   return app;
